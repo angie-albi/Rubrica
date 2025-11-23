eb754be6a3cb669689d9475eb1d09675a7d43964
angie <20054397@studenti.it>
2025-11-23 22:07:55 +0100

    add scrollPane

--- src/GUI/vista/ContentPanel.java
+++ src/GUI/vista/ContentPanel.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import modello.Contatto;
@@ -14,22 +15,29 @@ import modello.Rubrica;
 @SuppressWarnings("serial")
 public class ContentPanel extends JPanel {
 	private Rubrica model;
-	private JTextArea rubrica;
+	private JTextArea areaContatti;
 
 	public ContentPanel(Rubrica model) {
 		this.model = model;
 		setLayout(new BorderLayout());
 
 		// Area di testo non modificabile dall'utente
-		rubrica = new JTextArea(10, 20);
-		rubrica.setEditable(false);
-		rubrica.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+		areaContatti = new JTextArea(10, 20);
+		areaContatti.setEditable(false);
+		areaContatti.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 
-		JLabel label = new JLabel("Contenuto Rubrica " + model.getNome());
-
+		// Scorrimento 
+		JScrollPane scrollPane = new JScrollPane(areaContatti);
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        
+        // Titolo
+		JLabel label = new JLabel("Rubrica " + model.getNome());
+		add(areaContatti, BorderLayout.CENTER);
+		
 		add(label, BorderLayout.NORTH);
-		add(rubrica, BorderLayout.CENTER);
+		add(scrollPane, BorderLayout.CENTER);
 
+		// Corichiamo i contatti
 		aggiorna();
 	}
 
@@ -37,11 +45,21 @@ public class ContentPanel extends JPanel {
 		// Mostra tutti i contatti tramite la ricerca
 		ArrayList<Contatto> contatti = model.ricerca("");
 
-		rubrica.setText("NOME\tNUMERO TELEFONICO\n");
-		for (Contatto c : contatti) {
-			rubrica.append(c.getNome() + "\t" + c.getNumeroTel() + "\n");
-//			rubrica.append(c.toString());
-		}
+		if (contatti.isEmpty()) {
+            areaContatti.setText("\n  (Nessun contatto presente)");
+        } else {
+            // Ciclo for-each: per ogni Contatto 'c' nella lista
+            for(Contatto c : contatti) {
+                // c.toString() restituisce "Nome: Numero" (definito nel Modello)
+                areaContatti.append(" " + c.toString() + "\n");
+                areaContatti.append(" -------------------------\n"); // Separatore estetico
+            }
+        }
+		
+//		areaContatti.setText("NOME\tNUMERO TELEFONICO\n");
+//		for (Contatto c : contatti) {
+//			areaContatti.append(c.getNome() + "\t" + c.getNumeroTel() + "\n");
+//		}
 
 	}
 }
