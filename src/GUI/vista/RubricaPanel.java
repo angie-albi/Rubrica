8a7d50e428f4834213945bd7d8f9721907b03a78
angie <20054397@studenti.it>
2025-11-24 16:07:44 +0100

    Set Border RubricaPanel

--- src/GUI/vista/RubricaPanel.java
+++ src/GUI/vista/RubricaPanel.java
@@ -2,6 +2,7 @@ package GUI.vista;
 
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import GUI.controllo.ControlloRubrica;
 import modello.Rubrica;
@@ -9,7 +10,9 @@ import modello.Rubrica;
 public class RubricaPanel extends JPanel {
 
 	public RubricaPanel(Rubrica model) {
+		// Set Layout
 		setLayout(new BorderLayout());
+		setBorder(new EmptyBorder(5, 15, 15, 15));
 		
 		// Vista e Controllo
 		ContentPanel contenutoRubrica = new ContentPanel(model);
